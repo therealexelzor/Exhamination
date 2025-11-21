7535887b76182059f99b42154be72b0e985f01a3
therealexelzor <emil.exelzor@gmail.com>
2025-11-21 09:31:56 +0100

    Menu

---
+++ a25emina/Assets/Scripts/Scenechange.cs
@@ -0,0 +1,12 @@
+using UnityEngine;
+using UnityEngine.SceneManagement;
+public class NewMonoBehaviourScript : MonoBehaviour
+{
+   public void GoToSceneTwo()
+   {
+      SceneManager.LoadScene("Game")
+   }
+    
+    
+    
+}
